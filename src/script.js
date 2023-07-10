45d42168da73537b0c177f3695ed0c5d4cee7631
aamshodiqulmunir <aamshodiqulmunir@gmail.com>
2023-07-10 14:24:34 +0800

    Membuat Responsive Homepage

--- src/script.js
+++ src/script.js
@@ -3,6 +3,8 @@ const registerLink = document.querySelector('.register-link');
 const loginlink = document.querySelector('.login-link');
 const loginButton = document.querySelector('.btnloginPopup');
 const closeButton = document.querySelector('.icon-close');
+const hamburgerMenu = document.getElementsByClassName('hamburger-menu')[0];
+const navbar = document.getElementsByClassName('navbar')[0];
 
 registerLink.onclick = () => {
     wrapper.classList.add('active');
@@ -20,3 +22,7 @@ closeButton.onclick = () => {
     wrapper.classList.remove('active-popup');
     wrapper.classList.remove('active');
 }
+
+hamburgerMenu.addEventListener('click',() => {
+    navbar.classList.toggle('active')
+})
